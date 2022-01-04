0369ff076073c3364437a41cc435aaf2bc12e946
TungDinh15 <dinhthanhtung150200@gmail.com>
2022-01-04 13:05:19 +0700

    Change Socket.io to socket.io

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
 
-const io = require("Socket.io")(server, {
+const io = require("socket.io")(server, {
     cors: {
         origin: "*",
         methods: ["GET", "POST"]
